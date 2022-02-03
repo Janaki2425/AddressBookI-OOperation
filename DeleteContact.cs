396b2899267d5d84950637e7e06df546f8361950
Janaki2425 <96913266+Janaki2425@users.noreply.github.com>
2022-02-04 01:45:01 +0530

    Delete contact

---
+++ DeleteContact.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbook
+{
+    internal class DeleteContact
+    {
+        public void DeleteName()
+        {
+            if (AddContact.AddressBook.Count > 0)
+            {
+                Console.Write("Enter name of contact you want to delete: ");
+                string deleteName = Console.ReadLine();
+
+
+                foreach (var item in AddContact.AddressBook)
+                {
+                    if (deleteName.ToLower() == item.FirstName.ToLower())
+                    {
+                        AddContact.AddressBook.Remove(item);
+                        Console.WriteLine(deleteName + "'s Contact is successfully deleted.");
+                        break;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine(deleteName + " does not exist in Address Book.");
+                    }
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("Address Book is empty.");
+            }
+        }
+    }
+}
+
+
