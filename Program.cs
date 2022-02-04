27947753221319087c58393c4cd84a8775860efa
Janaki2425 <96913266+Janaki2425@users.noreply.github.com>
2022-02-04 16:39:51 +0530

    main

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace addressbook
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            ContactDetails contactDetail = new ContactDetails();
+            contactDetail.Details();
+           AddContact addNewContact = new AddContact();
+            addNewContact.EnterInput();
+            addNewContact.ViewTheDetails();
+            EditContact editDetails = new EditContact();
+            editDetails.EditName();
+            DeleteContact deleteDetails = new DeleteContact();
+            deleteDetails.DeleteName();
+            ViewPerson viewPerson=new ViewPerson();
+            viewPerson.ViewTheDetails();
+
+            
+            
+
+        }
+    }
+}
+        
+
+
+            
+
+
+            
+
+
+
+
+
+
+    
+
+
+
+    
+
