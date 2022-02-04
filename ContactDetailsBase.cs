2ff39836bb243c3da840f36fa7fde9d6db37b9ec
Janaki2425 <96913266+Janaki2425@users.noreply.github.com>
2022-02-04 16:36:12 +0530

    getting input

---
+++ ContactDetailsBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace addressbook
+{
+    public class ContactDetailsBase
+    {
+        public void Details()
+        { }
+        public string FirstName;
+        public string LastName;
+        public string Email;
+        public string PhoneNumber;
+        public string Address;
+        public string City;
+        public string ZipCode;
+        public string state;
+    }
+}
+
+        
+        
+       
+
+        
+    
+
+
